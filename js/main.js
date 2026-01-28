ed4cfc9b761e7a7b0f44c32e160c29d643381e31
sulaymon-hacker <sodiqovsulaymon@gmail.com>
2026-01-28 17:37:26 +0300

    jumping

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   navList.classList.remove("active");
 });
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  // Sahifa 50px pastga tushganda 'sticky' klassini qo'shadi
+  if (window.scrollY > 50) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
